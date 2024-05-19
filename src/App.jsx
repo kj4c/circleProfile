8b1a79c03ff40c0fa2eafef3864cdf5b80161702
kj4c <lowkhyejac@gmail.com>
2024-05-19 23:29:35 +1000

    added about me page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useNavigate } from "react-router-dom";
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  let navigate = useNavigate();
+  const routeChange = (newPath) => {
+    navigate(path)
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="navBar">
+        <button onClick = {() => routeChange('/')} className = "navButton">
+          Home
+        </button>
+        <button className = "navButton">
+          About Me
+        </button>
+        <button className = "navButton">
+          Hobbies
+        </button>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
